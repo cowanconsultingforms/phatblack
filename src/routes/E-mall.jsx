c857199e218afebdf8670605d3a64e481ac13ba4
Zhi Huang Li <zli8362@bths.edu>
2024-03-13 09:10:47 -0400

    Pages JSX Files

---
+++ src/routes/E-mall.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Emall() {
+    return (
+        <div>
+            <h1>E-Mall</h1>
+        </div>
+    )
+};
+
+export default Emall; 
---
+++ src/routes/PB-fashion.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PBfashion() {
+    return (
+        <div>
+            <h1>PB-Fashion</h1>
+        </div>
+    )
+};
+
+export default PBfashion; 
