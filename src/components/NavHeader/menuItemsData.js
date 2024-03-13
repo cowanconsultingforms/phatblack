13472a154778f937f0f9d5c19aa07aa63de29971
Zhi Huang Li <zli8362@bths.edu>
2024-03-13 12:49:21 -0400

    menu listings

---
+++ src/components/NavHeader/menuItemsData.js
@@ -0,0 +1,61 @@
+export const menuItemsData = [
+    {
+      title: "Home",
+      url: "/"
+    },
+    {
+      title: "News",
+      url: "/zine"
+    },
+    {
+      title: "Entertainment",
+      url: "#",
+      submenu: [
+        {
+          title: "E-TV",
+          url: "/tv"
+        },
+        {
+          title: "E-Radio",
+          url: "/radio"
+        },
+        {
+          title: "E-Gaming",
+          url: "/gaming"
+        },
+        {
+          title: "Defenders of Hip Hop",
+          url: "/defendersofhiphop"
+        },
+        {
+          title: "PB-Digital",
+          url: "/pbdigital"
+        },
+        {
+          title: "PB-Music",
+          url: "/pbmusic"
+        }
+      ]
+    },
+    {
+      title: "Fashion",
+      url: "/pbfashion"
+    },
+    {
+      title: "Events",
+      url: "/events"
+    },
+    {
+      title: "Subscribe",
+      url: "/subscribe"
+    },
+    {
+      title: "Shop",
+      url: "/mall"
+    },
+    {
+      title: "About",
+      url: "/about"
+    }
+  ];
+  
