694309c69f5f1cf8ded5d7d869e2cd0f1eea1c12
Zhi Huang Li <zli8362@bths.edu>
2024-03-15 13:40:44 -0400

    About page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import "./About.css";
 
 function About() {
     return (
-        <div>
-            <h1>About</h1>
-        </div>
-    )
-};
+        <div className="page-container">
+            <div className="page-content">
+                <h2>No. 1 Entertainment Platform</h2>
+                <h1>About Us</h1>
+                <div className="paragraph-container">
+                    <p>
+                        Welcome to PhatBlack.com – Your Cultural Soundstage.
+                        At PhatBlack.com we’re not just an entertainment platform,
+                        we’re a movement. Born from the vibrant streets of urban America,
+                        PhatBlack has grown into a global symphony of culture celebrating
+                        the richness of the urban African diaspora. Accordingly, Phatblack
+                        is dedicated to bringing to the fore the unique sounds, sights, and
+                        illustrative narratives reflective of the urban landscape.
+                    </p>
+                </div>
+            </div>
+        </div >
+    );
+}
 
-export default About; 
+export default About;
