b1fdbc43eef81ae5b2894f9109b8757e4d04902a
[Frank Yournet] <[frankYournet@gmail.com]>
2024-03-15 16:31:44 -0400

    Added styling changes and button useState

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { useState } from "react";
 import '../Styles/Home.css';
 
 
+
 function Home() {
+
+    const [buttonText, SetButtonText] = useState("GET MORE DETAILS")
+    let buttonPressed = true;
+
+    function getMoreHandler(){
+        buttonPressed = !buttonPressed;
+        if(buttonPressed == false){
+            SetButtonText("Button works.");
+        }
+        else if(buttonPressed==true){
+            SetButtonText("GET MORE DETAILS");
+        }
+        
+       
+    }
+    
     return (
         <div>
             <div className="imageDiv">
@@ -10,7 +28,7 @@ function Home() {
                 <h1>Home</h1>
                 <h2 className="orangeHeader">Join The Newtwork With</h2>
                 <h3 className="whiteHeader">LIVE AND ON DEMAND URBAN MUSIC & ENTERTAINMENT 24/7/365</h3>
-                <button className="homeButton">GET MORE DETAILS</button>
+                <button className="homeButton" onClick={getMoreHandler}>{buttonText}</button>
             </div>
             
         </div>
