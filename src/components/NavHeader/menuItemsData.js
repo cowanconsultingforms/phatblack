db8dfadd5ffb6585edb9591c94b37109bb68f3b5
stanley <stanleychen934@gmail.com>
2024-03-21 17:01:34 -0400

    In Progress

--- src/components/NavHeader/menuItemsData.js
+++ src/components/NavHeader/menuItemsData.js
@@ -1,64 +1,66 @@
 export const menuItemsData = [
-    {
-      title: "E-Zine",
-      url: "/zine"
-    },
-    {
-        title: "E-TV",
-        url: "/tv",
-    },
-    {
-      title: "E-Radio",
-      url: "/radio"
-    },  
-    {
-      title: "E-Gaming",
-      url: "/gaming"
-    },
-    {
-      title: "Defenders of Hip Hop",
-      url: "/defendersofhiphop"
-    },
-    {
-      title: "PB-Digital",
-      url: "/pbdigital"
-    },
-    {
-      title: "PB-Music",
-      url: "/pbmusic"
-    },
-    {
-      title: "PB-Fashion",
-      url: "/pbfashion"
-    },
-    {
-      title: "PB-Events",
-      url: "/events"
-    },
-    {
-      title: "PB-Communities",
-      url: "/pbcommunities"
-    },
-    {
-      title:"PB-Social",
-      url:"/pbsocial"
-    },
-    {
-      title: "Subscribe",
-      url: "/subscribe"
-    },
-    {
-      title: "Mall",
-      url: "/mall"
-    },
-    {
-      title: "About",
-      url: "/about"
-    },
-    {
-      title:"Contact Us",
-      url:"/contactus"
-    },
-    
-  ];
-  
+  {
+    title: "E-Zine",
+    url: "/zine"
+  },
+  {
+      title: "E-TV",
+      url: "/tv",
+  },
+  {
+    title: "E-Radio",
+    url: "/radio"
+  },  
+  {
+    title: "E-Gaming",
+    url: "/gaming"
+  },
+  {
+    title: "Defenders of Hip Hop",
+    url: "/defendersofhiphop"
+  },
+  {
+    title: "PB-Digital",
+    url: "/pbdigital"
+  },
+  {
+    title: "PB-Music",
+    url: "/pbmusic"
+  },
+  {
+    title: "PB-Fashion",
+    url: "/pbfashion"
+  },
+  {
+    title: "PB-Events",
+    url: "/events"
+  },
+  {
+    title: "PB-Communities",
+    url: "/pbcommunities"
+  },
+  {
+    title:"PB-Social",
+    url:"/pbsocial"
+  },
+  {
+    title: "Subscribe",
+    url: "/subscribe"
+  },
+  {
+    title: "PB-Mall",
+    url: "/pbmall"
+  },
+  {
+    title: "About",
+    url: "/about"
+  },
+  {
+    title: "FAQ",
+    url: "/faq"
+  },
+  {
+    title:"Contact Us",
+    url:"/contactus"
+  },
+];
---
+++ src/routes/FAQ.jsx
@@ -0,0 +1,9 @@
+function FAQ() {
+    return (
+        <div>
+            <h1>FAQ</h1>
+        </div>
+    )
+};
+
+export default FAQ; 
