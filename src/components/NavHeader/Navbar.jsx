11e3324bc294fd8ff33bcbb98800f73b43c9de17
stanleychen7 <stanleychen934@gmail.com>
2024-03-22 17:19:52 -0400

    Fixed arrow buttons for drag

--- src/components/NavHeader/Navbar.jsx
+++ src/components/NavHeader/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   // Initial depth level for menu items
   const depthLevel = 0;
   const [isNextDisable, setNextDisable] = useState(false);
-  const [isPrevDisable, setPrevDisable] = useState(false); // Initially disable
+  const [isPrevDisable, setPrevDisable] = useState(true); // Initially disable
 
   // Initialize dragX to track drag position
   const dragX = useMotionValue(0);
@@ -18,7 +18,7 @@ const Navbar = () => {
     dragX.set(newX);
 
     //Disable next button when reached the end
-    if (newX <= -((menuItemsData.length - 4) * 100)) {
+    if (newX <= -1000) {
       setNextDisable(true);
     } else {
       setNextDisable(false);
@@ -29,11 +29,11 @@ const Navbar = () => {
   };
 
   const handlePrevClick = () => {
-    const newX = dragX.get() + 100;
+    const newX = dragX.get() + 200;
     dragX.set(newX);
 
     //Disable prev button when reached the beginning
-    if (newX >= 0) {
+    if (newX >= 10) {
       setPrevDisable(true);
     } else {
       setPrevDisable(false);
@@ -46,7 +46,7 @@ const Navbar = () => {
   // Handle drag end
   const onDragEnd = () => {
     const x = dragX.get();
-    const threshold = -1; 
+    const threshold = 1; 
 
     if (Math.abs(x) > threshold) {
       if (x > 0 && !isPrevDisable) {
@@ -62,33 +62,34 @@ const Navbar = () => {
   return (
     <nav className="desktop-nav">
       {/* Render the desktop navigation */}
-      <div className="navbar-container">
-        <button className="arr left" onClick={handlePrevClick} disabled={isPrevDisable}>
-          &lt;
-        </button>
-        <ul className="menus">
-          {/* Carousel Content */}
-          {menuItemsData.map((menu, index) => (
-            <motion.div
-              className="carousel-container"
-              drag="x"
-              dragConstraints={{
-                left: -(menuItemsData.length - 4) * 100,
-                right: 0
-              }}
-              style={{
-                x: dragX
-              }}
-              onDragEnd={onDragEnd}
-            >
-              <MenuItems items={menu} key={index} depthLevel={depthLevel} />
-            </motion.div>
-          ))}
-        </ul>
-        <button className="arr right" onClick={handleNextClick} disabled={isNextDisable}>
-          &gt;
-        </button>
-      </div>
+        <div className="menu-container">
+          <div className="arr-button-container">
+            <button className="arr left" onClick={handlePrevClick} disabled={isPrevDisable}/>
+          </div>
+          <ul className="menus">
+            {/* Carousel Content */}
+            {menuItemsData.map((menu, index) => (
+              <motion.div
+                className="carousel-container"
+                drag="x"
+                dragConstraints={{
+                  left: -1100,
+                  right: 0,
+
+                }}
+                style={{
+                  x: dragX
+                }}
+                onDragEnd={onDragEnd}
+              >
+                <MenuItems items={menu} key={index} depthLevel={depthLevel} disabled={isPrevDisable}/>
+              </motion.div>
+            ))}
+          </ul>
+          <div className="arr-button-container">
+              <button className="arr right" onClick={handleNextClick} disabled={isNextDisable}/>
+          </div>
+        </div> 
     </nav>
   );
 };
