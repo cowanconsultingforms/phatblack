120d0ea84f3559a848979c4ad4b9b631951e1311
Dothat65 <meemoahmed2004@gmail.com>
2024-03-22 16:22:10 -0400

    making emall page

--- src/routes/PB-mall.jsx
+++ src/routes/PB-mall.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import Carousel from '../components/Carousel';
+import '../Styles/PbMall.css';
+
+
+
 
 function PBmall() {
     return (
-        <div>
-            <h1>PB-Mall</h1>
+        <div className="header-container">
+            <h1 className="Header">E-Mall</h1>
+            <div className="subtopics-container">
+                <h2 className="Subtopics">Featured</h2>
+                <h2 className="Subtopics">Clothes</h2>
+                <h2 className="Subtopics">Accessories</h2>
+                <h2 className="Subtopics">Home</h2>
+                <h2 className="Subtopics">Electronics</h2>
+                <h2 className="Subtopics">Personal Care</h2>
+                <h2 className="Subtopics">Miscellaneous</h2>
+            </div>
         </div>
-    )
+    );
 };
 
 export default PBmall; 
