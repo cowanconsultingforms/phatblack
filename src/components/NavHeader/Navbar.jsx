e00bc5a9dde60a0e32e7daa69dcab8e9823a5c95
[Frank Yournet] <[frankYournet@gmail.com]>
2024-03-22 11:28:57 -0400

    Navbar bubbles adjustment and scrolling

--- src/components/NavHeader/Navbar.jsx
+++ src/components/NavHeader/Navbar.jsx
@@ -5,27 +5,27 @@ import { useState } from "react";
 const Navbar = () => {
   // Initial depth level for menu items
   const depthLevel = 0;
-  const [currentMenuItems, setCurrentMenuItems] = useState(menuItemsData.slice(0,3));
+  const [currentMenuItems, setCurrentMenuItems] = useState(menuItemsData.slice(0,15));
   const [startIndex, setStartIndex] = useState(0);
   const [isNextDisable, setNextDisable] = useState(false);
   const [isPrevDisable, setPrevDisable] = useState(true);  //Initially disable
 
   //Handle Next Button Click
   const handleNextClick = () => {
-    const newStartIndex = startIndex + 3;
-    const newMenuItems = menuItemsData.slice(newStartIndex, newStartIndex + 3);
+    const newStartIndex = startIndex + 15;
+    const newMenuItems = menuItemsData.slice(newStartIndex, newStartIndex + 15);
     setCurrentMenuItems(newMenuItems);
     setStartIndex(newStartIndex);
     setPrevDisable(false);
 
     //Disable next button when reached the end
-    if (newStartIndex + 3 >= menuItemsData.length) {
+    if (newStartIndex + 15 >= menuItemsData.length) {
       setNextDisable(true);
     }
   }
 
   const handlePrevClick = () => {
-    const newStartIndex = startIndex - 3;
+    const newStartIndex = startIndex - 15;
     const newMenuItems = menuItemsData.slice(newStartIndex, startIndex);
     setCurrentMenuItems(newMenuItems);
     setStartIndex(newStartIndex);
@@ -41,15 +41,16 @@ const Navbar = () => {
   return (
     <nav className="desktop-nav">
       {/* Render the desktop navigation */}
-      
+      <button className="arr left" onClick={handlePrevClick} disabled={isPrevDisable}></button>
       <ul className="menus">
         {/* Map through menu items and render MenuItems component */}
-        <button className="arr left" onClick={handlePrevClick} disabled={isPrevDisable}></button>
+        
         {currentMenuItems.map((menu, index) => (
           <MenuItems items={menu} key={index} depthLevel={depthLevel} />
         ))}
-        <button className= "arr right" onClick={handleNextClick} disabled={isNextDisable}></button>
+        
       </ul>
+      <button className= "arr right" onClick={handleNextClick} disabled={isNextDisable}></button>
     </nav>
   );
 };
