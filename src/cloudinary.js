17745e563b45551feab732d27049ade08f784599
salmjd <106707092+salmjd@users.noreply.github.com>
2024-07-24 15:51:06 -0400

    Update cloudinary.js

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -1,9 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 const cloudinary = new Cloudinary({
-    cloud_name: 'dxptlsn6t',
-    api_key: '163944492188582',
-    api_secret: 'F3K_wMD6Yky_c_Umgj26CkhbVb8'
+
 });
 
 export default cloudinary;
